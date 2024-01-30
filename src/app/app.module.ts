a6898d5cd59c5f9f08f260e8b058600934c352e7
Ashlyn Godwin <ashlyn.godwin27@gmail.com>
2024-01-30 09:00:38 -0600

    Initial Upload, Header finished, navbar finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageheaderComponent } from './pageheader/pageheader.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageheaderComponent,
+    NavbarComponent,
+    HomepageComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
